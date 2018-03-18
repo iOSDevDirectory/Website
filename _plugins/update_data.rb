5cd2ce0350ecfcd753464855efffc8b1c5c58c75
Dave Verwer <dave.verwer@gmail.com>
2018-03-18 23:55:11 +0000

    Changed update data plugin to cope with the new data format.

--- _plugins/update_data.rb
+++ _plugins/update_data.rb
@@ -7,18 +7,20 @@ Jekyll::Hooks.register :site, :after_init do |site|
   # Download the data and parse into JSON
   uri = URI("https://raw.githubusercontent.com/daveverwer/iOSDevDirectory/master/sites.json")
   response = Net::HTTP.get(uri)
-  categories = JSON.parse(response)
+  content = JSON.parse(response)
 
   # Sort the data, case insensitive!
-  categories.each do |category|
-    category["sites"].sort_by! { |site| site["title"].downcase }
+  content.each do |language|
+    language["categories"].each do |category|
+      category["sites"].sort_by! { |site| site["title"].downcase }
+    end
   end
 
   # Write the JSON as part of the site before generation starts
   json_path = File.join(site.source, "/_data/")
-  json_file = "categories.json"
+  json_file = "content.json"
   FileUtils.mkdir_p(json_path)
-  File.open("#{json_path}#{json_file}", "w") do |file|
-    file.write(JSON.pretty_generate(categories))
+    File.open("#{json_path}#{json_file}", "w") do |file|
+    file.write(JSON.pretty_generate(content))
   end
 end
