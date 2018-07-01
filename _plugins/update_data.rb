037d4e73997731abb2541b3531fd4342f49d0501
Dave Verwer <dave.verwer@gmail.com>
2018-07-01 22:06:48 +0100

    Better comment.

--- _plugins/update_data.rb
+++ _plugins/update_data.rb
@@ -4,19 +4,18 @@ require "json"
 Jekyll::Hooks.register :site, :after_init do |site|
   Jekyll.logger.info "Updating JSON Data: Blogs & Sites"
 
-  # Download the data and parse into JSON
-  uri = URI("https://raw.githubusercontent.com/daveverwer/iOSDevDirectory/master/blogs.json")
-  response = Net::HTTP.get(uri)
+  # Download and parse the blogs data
+  response = Net::HTTP.get(URI("https://raw.githubusercontent.com/daveverwer/iOSDevDirectory/master/blogs.json"))
   content = JSON.parse(response)
 
-  # Sort the data, case insensitive!
+  # Sort the blogs in each category, case insensitive!
   content.each do |language|
     language["categories"].each do |category|
       category["sites"].sort_by! { |site| site["title"].downcase }
     end
   end
 
-  # Write the JSON as part of the site before generation starts
+  # Write the local copy of the blogs data
   json_path = File.join(site.source, "/_data/")
   json_file = "content.json"
   FileUtils.mkdir_p(json_path)
